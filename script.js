75ad626d63f456e3611d0fa168a4812ac925b02e
Pawel <skrobskipawel@gmail.com>
2022-10-17 11:37:26 +0200

    First interface done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon'
 const SPECIES_URL = 'https://pokeapi.co/api/v2/pokemon-species'
 
+const pokemons = [];
+
 let nav_bar = document.getElementById('nav');
 
 
@@ -18,12 +20,49 @@ async function call(url = POKEMON_URL) {
 async function main(url) {
     let data = await call(url);
     data.results.map((el, index) => {
+        pokemons.push(el);
         var li = document.createElement('li')
-        li.innerHTML = `${index} ${el.name}`
+        li.innerHTML = `${('0000' + (index + 1)).slice(-4)}  ${el.name}`
         document.getElementById('nav').appendChild(li);
+        li.addEventListener('click', (event) => {
+            main_spec(POKEMON_URL + '/' + el.name, index);
+            desc_spec(SPECIES_URL + '/' + el.name);
+        })
+
     })
-    // expected output: "resolved"
+}
+
+async function main_spec(url, index) {
+    let data = await call(url);
+
+    let height_div = document.getElementById("height");
+    let weight_div = document.getElementById("weight");
+    let name_div = document.getElementById("pokemon-name");
+    let img_div = document.getElementById("pokemon-image");
+
+    [height_div.textContent, weight_div.textContent, name_div.textContent] = ['', '', ''];
+
+    if (data !== undefined) {
+        height_div.textContent += data.height;
+        weight_div.textContent += data.weight;
+        name_div.textContent += `${('0000' + (index + 1)).slice(-4)}  ${data.name}`;
+        img_div.src = data.sprites.front_default;
+    }
+}
+
+async function desc_spec(url, index) {
+    let data = await call(url);
+    let desc_div = document.getElementById("description");
+
+    desc_div.textContent = ''
+
+    if (data !== undefined) {
+        return desc_div.textContent += data.flavor_text_entries[11].flavor_text;
+    }
+
+    desc_div.textContent += 'No description provided for that pokemon';
 }
 
 
-main(POKEMON_URL)
+main(POKEMON_URL + '?limit=100000&offset=0')
+
